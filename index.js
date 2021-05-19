411ec8b98da79354aa376aa26ff7670855b73b75
Ankit verma <av27sept@gmail.com>
2021-05-19 22:24:30 +0530

    dep

--- index.js
+++ index.js
@@ -17,20 +17,20 @@ const db = require('./config/mongoose.js');  // using mongoose to connect to mon
 const app = express();
 
 
-const whitelist = ['http://localhost:3000', 'http://localhost:8000/','http://localhost:8000/api/', 'https://studentregistrations.herokuapp.com']
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** Origin of request" + origin)
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable")
-      callback(null, true)
-    } else {
-      console.log("Origin rejected")
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use(cors(corsOptions));
+// const whitelist = ['http://localhost:3000', 'http://localhost:8000/','http://localhost:8000/api/', 'https://studentregistrations.herokuapp.com']
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     console.log("** Origin of request" + origin)
+//     if (whitelist.indexOf(origin) !== -1 || !origin) {
+//       console.log("Origin acceptable")
+//       callback(null, true)
+//     } else {
+//       console.log("Origin rejected")
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
+app.use(cors());
 
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
